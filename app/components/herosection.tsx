931432a98dc4d9042f46b627f2247f21708e3cf2
Error-614 <mahlabazam614@gmail.com>
2024-11-25 11:47:16 +0500

    second commit

--- app/components/herosection.tsx
+++ app/components/herosection.tsx
@@ -7,7 +7,7 @@ export default function Herosection(){
                 <div className="font-sans ">
                     <h1 className="text-center text-2xl font-bold p-3">Water Perfume For Men</h1>
                     <p className="text-sm lg:text-base text-center px-5 ">
-                    The scent embodies the spirit of the ocean's depths. This fragrance takes you to the calm waves of the Mediterranean Sea with its energizing top notes of bergamot and marine accord.As the fragrance evolves, rich and aromatic hints of cypress emerge, evoking the rugged beauty of the coastal landscape. The heart of fragrance is anchored by deep and notes of lavender, patchouli and ambergris, creating a sense of depth and intensity.
+                    The scent embodies the spirit of the ocean&apos;s depths. This fragrance takes you to the calm waves of the Mediterranean Sea with its energizing top notes of bergamot and marine accord.As the fragrance evolves&lsquo; rich and aromatic hints of cypress emerge&lsquo; evoking the rugged beauty of the coastal landscape. The heart of fragrance is anchored by deep and notes of lavender&lsquo; patchouli and ambergris&lsquo; creating a sense of depth and intensity.
                     </p>
                     <p className="font-semibold text-center p-2">PKR 3,790</p>
                     <div className="flex justify-center items-center p-2">
@@ -21,38 +21,7 @@ export default function Herosection(){
                 </div>
 
             </div>
-        {/* <div className="flex md:flex-row flex-col justify-around   ">
-            
-        <div className="md:w-5/12 w-full mt-10 mb-10">
-        <Image src="/memory.jpg" alt="memory 1 " width={500}height={500}/>
-        <p className="text-center pt-10">pkr 5000</p>
-        <div className=" flex justify-center items-center my-3">
-        <button className="px-10 py-4 bg-blue-600 text-white rounded-md mx-auto">
-            buy now
-        </button>
-        </div>
-    </div>
-
-    <div className="md:w-5/12 w-full mt-10 mb-10">
-        <Image src="/memory.jpg" alt="memory 1 " width={500}height={500}/>
-        <p className="text-center pt-10">pkr 5000</p>
-        <div className=" flex justify-center items-center my-3">
-        <button className="px-10 py-4 bg-blue-600 text-white rounded-md mx-auto">
-            buy now
-        </button>
-        </div>
-    </div>
-
-    <div className="md:w-5/12 w-full mt-10 mb-10">
-        <Image src="/memory.jpg" alt="memory 1 " width={500}height={500}/>
-        <p className="text-center pt-10">pkr 5000</p>
-        <div className=" flex justify-center items-center my-3">
-        <button className="px-10 py-4 bg-blue-600 text-white rounded-md mx-auto">
-            buy now
-        </button>
-        </div>
-    </div>
-    </div>   */}
+    
     </div>
     )
 }
